4cba1d6d37c72db4c877f9b0fc0f08cb605f0e27
davidssantoss <davidsteven.santos9@gmail.com>
2019-03-27 01:31:03 -0500

    Almost done :D

---
+++ Ejercicio ciencias Ec. temporal/script.js
@@ -0,0 +1,25 @@
+function burbujaOrden(){
+  var num = document.getElementById('cant').value;
+  var arregloA = new Array(num);
+  for (var i = 1; i < num; i++) {
+    var random = Math.floor((Math.random() * 100) + 1);
+    arregloA.push(random);
+  }
+  document.getElementById('numCreados').value = arregloA;
+  var c = 1;
+  for (var j = 0; j < num - 1; j++) {
+    c = c + 5;
+    for (var k = j + 1; k < num; k++) {
+      c = c + 5;
+      if (arregloA[j] > arregloA[k]) {
+        var t = arregloA[j];
+        arregloA [j] = arregloA[k];
+        arregloA [k] = t;
+      }
+    }
+    c = c + 1;
+  }
+  document.getElementById('numOrd').value = arregloA;
+  document.getElementById('cantOE').value = c;
+  document.getElementById('ecu').value = ((num * (num - 1) - (num - 1) - ((num - 2) * (num - 1)) / 2) * 12 + 6 * (num - 1) + 3);
+}
