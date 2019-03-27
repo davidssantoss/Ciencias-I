4f0b6c97029516189bd2987269d178be52cea8be
davidssantoss <davidsteven.santos9@gmail.com>
2019-03-27 02:19:44 -0500

    add another function desc

--- Ejercicio ciencias Ec. temporal/script.js
+++ Ejercicio ciencias Ec. temporal/script.js
@@ -8,20 +8,55 @@ function burbujaOrden(){
   document.getElementById('numCreados').value = arregloA;
   var c = 1;
   for (var j = 0; j < num - 1; j++) {
-    c += c + 5;
+    c = c + 5;
+
     for (var k = j + 1; k < num; k++) {
-      c += c + 5;
+      c = c + 6;
+
       if (arregloA[j] > arregloA[k]) {
         var t = arregloA[j];
         arregloA [j] = arregloA[k];
         arregloA [k] = t;
-        c += c+7;
+        c = c+7;
       }
     }
-    c += c + 1;
+    c = c + 1;
   }
-  c += c + 2;
+  c = c + 2;
   document.getElementById('numOrd').value = arregloA;
   document.getElementById('cantOE').value = c;
-  document.getElementById('ecu').value = ((num * (num - 1) - (num - 1) - ((num - 2) * (num - 1)) / 2) * 12 + 6 * (num - 1) + 3);
+  document.getElementById('ecu').value = ((num * (num - 1) - (num - 1) - (((num - 2) * (num - 1)) / 2)) * 12 + 6 * (num - 1) + 3);
 }
+/*
+
+function descendente(){
+  var num = document.getElementById('cant').value;
+  var arregloA = new Array(num);
+  for (var i = 0; i < num; i++) {
+    var x = 20;
+    x = x - i;
+    arregloA.push(x);
+  }
+  document.getElementById('numCreados').value = arregloA;
+  var c = 1;
+  for (var j = 0; j < num - 1; j++) {
+    c = c + 5;
+
+    for (var k = j + 1; k <= num; k++) {
+      c = c + 6;
+
+      if (arregloA[j] > arregloA[k]) {
+        var t = arregloA[j];
+        arregloA [j] = arregloA[k];
+        arregloA [k] = t;
+        c = c+7;
+      }
+    }
+    c = c + 1;
+  }
+  c = c + 2;
+  document.getElementById('numOrd').value = arregloA;
+  document.getElementById('cantOE').value = c;
+  document.getElementById('ecu').value = ((num * (num - 1) - (num - 1) - (((num - 2) * (num - 1)) / 2)) * 12 + 6 * (num - 1) + 3);
+
+}*/
