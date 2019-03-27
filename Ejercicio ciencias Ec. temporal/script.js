17dc8b6299c443e834bccff2930e412ac4feb180
davidssantoss <davidsteven.santos9@gmail.com>
2019-03-27 01:41:57 -0500

    c...

--- Ejercicio ciencias Ec. temporal/script.js
+++ Ejercicio ciencias Ec. temporal/script.js
@@ -8,17 +8,19 @@ function burbujaOrden(){
   document.getElementById('numCreados').value = arregloA;
   var c = 1;
   for (var j = 0; j < num - 1; j++) {
-    c = c + 5;
+    c += c + 5;
     for (var k = j + 1; k < num; k++) {
-      c = c + 5;
+      c += c + 5;
       if (arregloA[j] > arregloA[k]) {
         var t = arregloA[j];
         arregloA [j] = arregloA[k];
         arregloA [k] = t;
+        c += c+7;
       }
     }
-    c = c + 1;
+    c += c + 1;
   }
+  c += c + 2;
   document.getElementById('numOrd').value = arregloA;
   document.getElementById('cantOE').value = c;
   document.getElementById('ecu').value = ((num * (num - 1) - (num - 1) - ((num - 2) * (num - 1)) / 2) * 12 + 6 * (num - 1) + 3);
