cc50a57540f71f4f2b222417f0a8cf05325e4832
davidssantoss <davidsteven.santos9@gmail.com>
2019-04-05 18:08:21 -0500

    reduccion de gauss
    falta la traza

--- Determinante de una matriz/script.js
+++ Determinante de una matriz/script.js
@@ -1,22 +1,23 @@
-function deterMatriz() {
-  //var m[][];
+function getMatriz() {
+  var n = parseInt(document.getElementById('cantNum').value);
+  var m = new Array(n);
+  for (var i = 0; i < n; i++) {
+    m[i] = new Array(n);
+  }
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < n; j++) {
+      var input = "texto" + i + j;
+      m[i][j] = parseInt(document.getElementById(input).value);
+			//console.log(m[i][j]);
+    }
+  }
   for(var i = 0; i< n-1;i++){
     for(var j = i + 1;j < n;j++){
       for(var k = i + 1; k < n; k++){
-        //m[k][j] = (m[k][j] - ((m[k][i]*m[i][j])/m[i][i]));
+        m[k][j] = (m[k][j] - ((m[k][i]*m[i][j])/m[i][i]));
+        console.log(m[k][j]);
       }
     }
-  }
-}
-
-function getMatriz() {
-  var n = document.getElementById('cantNum').value;
-  var m = Array(n);
-  for (var i = 0; i < n; i++) {
-    m[i] = new Array[n];
-  }
-  for (var i = 0; i < 4; i++) {
-
   }
 }
 function generarMatriz() {
