dacf0343b394e76372b039404985784da25d69be
davidssantoss <davidsteven.santos9@gmail.com>
2019-05-01 16:33:42 -0500

    detalles...

--- QuickSort/script.js
+++ QuickSort/script.js
@@ -2,7 +2,7 @@ function quickSort() {
   var n = parseInt(document.getElementById('cantNum').value);
   var arregloA = new Array();
   for (var i = 0; i < n; i++) {
-    var random = Math.floor((Math.random() * 100) + 1);
+    var random = Math.floor((Math.random() * 900) + 1);
     arregloA[i] = random;
   }
   document.getElementById('numGen').value = arregloA;
