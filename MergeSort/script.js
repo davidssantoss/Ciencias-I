41e25a50650c8cf3d6c28cefbdfdacb91152c9b4
davidssantoss <davidsteven.santos9@gmail.com>
2019-05-01 18:12:02 -0500

    my version

--- MergeSort/script.js
+++ MergeSort/script.js
@@ -1,8 +1,9 @@
 function numRand() {
   var n = parseInt(document.getElementById('cantNum').value);
   var arregloA = new Array();
-  for (var i = 0; i < n; i++) {
+  for (var i = 1; i < n; i++) {
     var random = Math.floor((Math.random() * 900) + 1);
+    arregloA[0] = 0;
     arregloA[i] = random;
   }
   document.getElementById('numGen').value = arregloA;
