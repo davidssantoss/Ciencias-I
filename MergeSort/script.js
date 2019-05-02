c9c98f3f8bf8f40eb016ba33645fb936d7d31130
davidssantoss <davidsteven.santos9@gmail.com>
2019-05-01 19:08:02 -0500

    MergeSort hecho

--- MergeSort/script.js
+++ MergeSort/script.js
@@ -1,10 +1,51 @@
 function numRand() {
   var n = parseInt(document.getElementById('cantNum').value);
   var arregloA = new Array();
-  for (var i = 1; i < n; i++) {
+  for (var i = 0; i < n; i++) {
     var random = Math.floor((Math.random() * 900) + 1);
-    arregloA[0] = 0;
     arregloA[i] = random;
   }
   document.getElementById('numGen').value = arregloA;
+  document.getElementById('numOrd').value = ordenar(arregloA);
+}
+function ordenar(arreglo) {
+  if (arreglo.length <= 1) {
+    return arreglo;
+  }
+  var longitud = arreglo.length;
+  var mitad = parseInt(arreglo.length / 2);
+  var izq = new Array(mitad);
+  var der = new Array(longitud - mitad);
+  for (var i = 0; i < izq.length; i++) {
+    izq[i] = arreglo[i];
+  }
+  for (var i = 0; i < der.length; i++) {
+    der[i] = arreglo[i + izq.length];
+  }
+  izq = ordenar(izq);
+  der = ordenar(der);
+  return unir(izq, der);
+}
+function unir(izquierda, derecha) {
+  var result = new Array(izquierda.length + derecha.length);
+  var indIzq = 0;
+  var indDer = 0;
+  var indRes = 0;
+  while (indIzq < izquierda.length || indDer < derecha.length) {
+    if (indIzq < izquierda.length && indDer < derecha.length) {
+      if (izquierda[indIzq] <= derecha[indDer]) {
+        result[indRes++] = izquierda[indIzq++];
+      }
+      else {
+        result[indRes++] = derecha[indDer++];
+      }
+    }
+    else if (indIzq < izquierda.length) {
+      result[indRes++] = izquierda[indIzq++];
+    }
+    else if (indDer < derecha.length) {
+      result[indRes++] = derecha[indDer++];
+    }
+  }
+  return result;
 }
