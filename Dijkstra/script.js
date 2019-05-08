f9f5ceb50987db0dbb6c7f6f0b646ae79143cc7c
davidssantoss <davidsteven.santos9@gmail.com>
2019-05-08 17:45:32 -0500

    fixing mistakes

--- Dijkstra/script.js
+++ Dijkstra/script.js
@@ -1,4 +1,4 @@
-function punto(){
+function generarGrafo(){
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext("2d");
   var cw = canvas.width = 700,
@@ -8,25 +8,12 @@ function punto(){
 
   var dibujar = false;
   ctx.lineJoin = "round";
-  var contador = 0;
-  contador = canvas.addEventListener('click', function(evt) {
+  canvas.addEventListener('click', function(evt) {
     dibujar = true;
     var m = oMousePos(canvas, evt);
-    var arr = new Array(2);
-    var vertices = new Array();
     ctx.lineTo(m.x, m.y);
-    arr[0] = m.x;
-    arr[1] = m.y;
-    console.log("valor de x: " + m.x + " valor de y: " + m.y);
-    for (var i = 0; i < 5; i++) {
-      vertices[i] = arr;
-    }
-    //mirar como obtener la cantidad de vertices y generar la matriz
-    console.log(vertices);
     ctx.stroke();
-    contador += 1;
   }, false);
-  console.log("La cantidad de clicks son: " + contador);
 }
 function oMousePos(canvas, evt) {
   var ClientRect = canvas.getBoundingClientRect();
@@ -35,8 +22,32 @@ function oMousePos(canvas, evt) {
     y: Math.round(evt.clientY - ClientRect.top)
   }
 }
+var count_click = 0;
+function count_click_add() {
+
+  count_click += 1;
+  console.log(count_click);
+  return count_click;
+}
+function crearMatrizA() {
+  const divtextoCreados = document.getElementById('txtCreados');
+  const divmatrizGen1 = document.getElementById('MG1');
+  for (var i = 0; i < count_click + 1; i++) {
+		for (var j = 0; j < count_click + 1; j++) {
+      //input text de la matriz
+			var input = document.createElement("input");
+			input.id = "texto" + i + j;
+      input.classList.add('matrizAdy');
+      divtextoCreados.insertBefore(input, divmatrizGen1);
+
+		}
+		var println = document.createElement("br");
+		divtextoCreados.insertBefore(println, divmatrizGen1);
+	}
+}
 /*
 @link
 http://w3.unpocodetodo.info/lab/dibujar-a-mano-alzada.php
 para mirar
+enviar lo de musica al aardila807@gmail.com
 */
