839079a9cf2b51493628b3be018cbc1cf043a26f
davidssantoss <davidsteven.santos9@gmail.com>
2019-05-22 10:42:21 -0500

    inputs creados | Falta Raphson y gauss-jordan

--- Ecuaciones_recurrencia/script.js
+++ Ecuaciones_recurrencia/script.js
@@ -1,3 +1,30 @@
 function crearInputs() {
-
+  crearCoeficientes();
+  crearCondicionesIniciales();
+}
+function crearCoeficientes() {
+  var n = parseInt(document.getElementById('cantInp').value);
+  const divPrimero = document.getElementById('txtCreados');
+  const divSegundo = document.getElementById('coef');
+  for (var i = 0; i <= n; i++) {
+    var input = document.createElement("input");
+    input.id = "coeficiente" + i ;
+    input.classList.add('txt');
+    divPrimero.insertBefore(input, divSegundo);
+  }
+}
+function crearCondicionesIniciales() {
+  var n = parseInt(document.getElementById('cantInp').value);
+  const divPrimero = document.getElementById('textos');
+  const divSegundo = document.getElementById('condiciones');
+  for (var i = 0; i < n; i++) {
+    for (var j = 0; j < 2; j++) {
+      var input = document.createElement("input");
+      input.id = "condicion" + i ;
+      input.classList.add('txt');
+      divPrimero.insertBefore(input, divSegundo);
+    }
+    var println = document.createElement("br");
+		divPrimero.insertBefore(println, divSegundo);
+  }
 }
